c9e41747a1e308c2fc826362af05e64d64da898b
Sebastian1812 <115327871+Sebastian1812@users.noreply.github.com>
2024-05-17 16:29:00 -0700

    Add files via upload

--- perfil_usuarioestudiante.php
+++ perfil_usuarioestudiante.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cursos de Ingeniería en Computación</title>
+    <title>Perfil de Usuario</title>
     <?php include 'include_login.php'; ?>
 </head>
 
