d0a24cf6399bd8d00088999f4dfed8c061013cb9
Sebastian1812 <115327871+Sebastian1812@users.noreply.github.com>
2024-05-17 17:55:00 -0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,42 +1,47 @@
 <style>
     .footer {
-    background-color: #00a74f;
-    padding: 10px;
-    text-align: center;
-    width: 100%;    
-    margin-top: auto;
-    display: flex;
-    justify-content:space-between;
-}
+        background-color: #00a74f;
+        padding: 10px;
+        width: 100%;    
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: auto;
+    }
 
+    .footer-text {
+        flex-grow: 1;
+        text-align: center;
+    }
 
+    .footer img {
+        height: 30px;
+        margin-left: 10px;
+    }
 
-.footer img {
-    height: 30px;
-    margin-right: 30px;
-}
-
-.footer h3 {
-    margin: 0;
-    color: #3b3b3b;
-    font-size: 20px;
-    font-family: "Montserrat", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: 250;
-    font-style: normal;
-}
+    .footer h3 {
+        margin: 0;
+        color: #3b3b3b;
+        font-size: 20px;
+        font-family: "Montserrat", sans-serif;
+        font-weight: 250;
+        font-style: normal;
+    }
 </style>
 
 <div class="footer">
-            <h3>UNIVERSIDAD AUTÓNOMA DE BAJA CALIFORNIA<br>@COPYRIGHT 2024</h3>
-            <a href="https://www.facebook.com/UABCInstitucional" target="_blank">
-                <img src="img\facebookLogo.png" alt="Facebook">
-            </a>
-            <a href="mailto:comunicacion.uabc@uabc.edu.mx">
-                <img src="img\gmailLogo.png" alt="Gmail">
-            </a>
-            <a href="https://www.uabc.mx/" target="_blank">
-                <img src="img\weblogo.png" alt="Web">
-            </a>
-            
+    <div class="footer-text">
+        <h3>UNIVERSIDAD AUTÓNOMA DE BAJA CALIFORNIA<br>@COPYRIGHT 2024</h3>
+    </div>
+    <div class="footer-icons">
+        <a href="https://www.facebook.com/UABCInstitucional" target="_blank">
+            <img src="img/facebookLogo.png" alt="Facebook">
+        </a>
+        <a href="mailto:comunicacion.uabc@uabc.edu.mx">
+            <img src="img/gmailLogo.png" alt="Gmail">
+        </a>
+        <a href="https://www.uabc.mx/" target="_blank">
+            <img src="img/weblogo.png" alt="Web">
+        </a>
+    </div>
 </div>
